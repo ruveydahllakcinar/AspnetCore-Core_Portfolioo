9ae750a7351b0a38b2c1451738d71f8bfd81c2ba
Ruveyda Hilal <rhakcinar@gmail.com>
2023-01-31 00:59:13 +0300

    finished admin message

--- Core_Portfolio/Controllers/AdminMessageController.cs
+++ Core_Portfolio/Controllers/AdminMessageController.cs
@@ -1,5 +1,7 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -31,6 +33,43 @@ namespace Core_Portfolio.Controllers
         {
             var values = writerMessageManager.TGetByID(id);
             return View(values);
+        }
+
+        public IActionResult AdminMessageDelete(int id)
+        {
+            string mail;
+            mail = "akcinarruveyda@gmail.com";
+            var values = writerMessageManager.TGetByID(id);
+            if (values.Receiver ==mail)
+            {
+                writerMessageManager.TDelete(values);
+                return RedirectToAction("ReceiverMessageList");
+
+            }
+            else
+            {
+                writerMessageManager.TDelete(values);
+                return RedirectToAction("SenderMessageList");
+            }
+        }
+        
+        
+        [HttpGet]
+        public IActionResult AdminMessageSend()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AdminMessageSend(WriterMessage wm)
+        {
+            wm.Sender = "akcinarruveyda@gmail.com";
+            wm.SenderName = "Ruveyda Hilal AKÇINAR";
+            wm.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            Context c = new Context();
+            var usernamesurname = c.Users.Where(x => x.Email == wm.Receiver).Select(y => y.Name + " " + y.Surname).FirstOrDefault();
+            wm.ReceiverName = usernamesurname;
+            writerMessageManager.TAdd(wm);
+            return RedirectToAction("SenderMessageList");
         }
     }
 }
